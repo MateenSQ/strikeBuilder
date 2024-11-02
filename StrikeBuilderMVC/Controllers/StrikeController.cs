05916a8bc09e1a86d71dca2755857e24be4a9d7f
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2024-11-02 15:51:59 +0000

    added controller, view and pdf of lo-fi

---
+++ StrikeBuilderMVC/Controllers/StrikeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StrikeBuilderMVC.Controllers
+{
+    public class StrikeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
